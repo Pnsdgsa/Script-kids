9163782997f235f6837ad55a3c8347b2e948a90c
Pnsdgsa <hfjthyhjtjyfbrmyghfhgh@gmail.com>
2025-10-02 13:50:07 +0700

    Create Why are you here? There's nothing here.lua

---
+++ Scripthub/Darahub/UHH-OHH-Sorry-This-Game-is-Not-Supported/Why are you here? There's nothing here.lua
@@ -0,0 +1,40 @@
+local WindUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Footagesus/WindUI/main/dist/main.lua"))()
+
+local Localization = WindUI:Localization({
+    Enabled = true,
+    Prefix = "loc:",
+    DefaultLanguage = "en",
+    Translations = {
+        ["en"] = {
+            ["LIB_DESC"] = "<b><font color='#FF0000'>ERROR⚠️: THIS GAME IS NOT SUPPORTED. Please make sure look at the source code and try again later</font></b>"
+        }
+    }
+})
+
+WindUI.TransparencyValue = 0.2
+WindUI:SetTheme("Dark")
+
+local function gradient(text, startColor, endColor)
+    local result = ""
+    for i = 1, #text do
+        local t = (i - 1) / (#text - 1)
+        local r = math.floor((startColor.R + (endColor.R - startColor.R) * t) * 255)
+        local g = math.floor((startColor.G + (endColor.G - startColor.G) * t) * 255)
+        local b = math.floor((startColor.B + (endColor.B - startColor.B) * t) * 255)
+        result = result .. string.format('<font color="rgb(%d,%d,%d)">%s</font>', r, g, b, text:sub(i, i))
+    end
+    return result
+end
+
+WindUI:Popup({
+    Title = gradient("Dara Hub", Color3.fromHex("#6A11CB"), Color3.fromHex("#2575FC")),
+    Icon = "sparkles",
+    Content = "loc:LIB_DESC",
+    Buttons = {
+        {
+            Title = "Close",
+            Variant = "Primary",
+            Callback = function() end
+        }
+    }
+})
