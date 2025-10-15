8dcf353f5314e4e2b4e0c9b5fc0bcfc366667110
Pnsdgsa <hfjthyhjtjyfbrmyghfhgh@gmail.com>
2025-10-16 06:19:40 +0700

    Update main-loader.lua

--- Scripthub/main-loader.lua
+++ Scripthub/main-loader.lua
@@ -57,6 +57,8 @@ if selectedScript then
             return loadstring(game:HttpGet(selectedScript.url))()
         end)
         if not success then
+            --game.Players.LocalPlayer:Kick("your nigga executor is ass 💀🙏! get a better executor dawg")
+           
             game:GetService("StarterGui"):SetCore("SendNotification", {
                 Title = "Bad Executor Detected",
                 Text = "Failed to execute the script, http failed" .. selectedScript.name .. " script: " .. tostring(result),
