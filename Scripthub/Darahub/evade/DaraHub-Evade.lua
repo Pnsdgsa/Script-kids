136f33ad8fc90872a404efc07177ea26e8513073
Pnsdgsa <hfjthyhjtjyfbrmyghfhgh@gmail.com>
2025-10-17 08:26:43 +0700

    Update DaraHub-Evade.lua

--- Scripthub/Darahub/evade/DaraHub-Evade.lua
+++ Scripthub/Darahub/evade/DaraHub-Evade.lua
@@ -5629,6 +5629,7 @@ end)
 
 --[[the part of loadstring prevent error]]
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Pnsdgsa/Script-kids/refs/heads/main/Scripthub/Darahub/evade/evade%20leaderboard%20button.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Pnsdgsa/Script-kids/refs/heads/main/Scripthub/Darahub/evade/Reload%20and%20Front%20View%20button.lua'))()
 
 local script = loadstring(game:HttpGet('https://raw.githubusercontent.com/Pnsdgsa/Script-kids/refs/heads/main/Scripthub/Darahub/evade/TimerGUI-NoRepeat'))()
 --filename "TimerGUI" code inside:
