5b11e1167564b3ff07c88a37883c3645b82f1317
Pnsdgsa <hfjthyhjtjyfbrmyghfhgh@gmail.com>
2026-01-08 10:02:33 +0700

    Update main-loader.lua

--- Scripthub/main-loader.lua
+++ Scripthub/main-loader.lua
@@ -1,2 +1,1 @@
-setclipboard('loadstring(game:HttpGet("https://darahub.vercel.app/main.lua"))()')
-game.Players.LocalPlayer:Kick("Script has been moved, please execute the new script from your clipboard")
+game.Players.LocalPlayer:Kick("Script outdated Please Revisit https://darahub.vercel.app")
