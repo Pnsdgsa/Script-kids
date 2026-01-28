84b0ced742c02f95d3e49332d017373e2ff06ae2
Pnsdgsa <hfjthyhjtjyfbrmyghfhgh@gmail.com>
2026-01-29 02:42:49 +0700

    Update Advanced Server Hop.lua

--- Advanced Server Hop.lua
+++ Advanced Server Hop.lua
@@ -1,1 +1,1 @@
-game.Players.LocalPlayer:Kick("Serverhop clsoed, This script will be back sooner. Reason: This GitHub repo will be deleted in 7day")
+game.Players.LocalPlayer:Kick("Serverhop clsoed, This script will be back sooner. Reason: This GitHub repo will be deleted in 6 day")
